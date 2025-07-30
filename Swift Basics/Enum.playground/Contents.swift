a5aa181fdc543588b5d2387ca223defb70b4f982
배정만 <jungman82@gmail.com>
2025-07-30 10:28:42 +0900

    CompassDirection 열거형 추가 및 방향에 따른 출력 로직 구현

--- Swift Basics/Enum.playground/Contents.swift
+++ Swift Basics/Enum.playground/Contents.swift
@@ -1,1 +1,35 @@
 
+enum CompassDirection {
+  case north
+  case south
+  case east
+  case west
+}
+
+let direction: CompassDirection = CompassDirection.south
+
+print("The direction is \(direction)")
+
+if direction == .south {
+  print("We are heading south!")
+} else if direction == .north {
+  print("We are heading north!")
+} else if direction == .east {
+  print("We are heading east!")
+} else if direction == .west {
+  print("We are heading west!")
+} else {
+  print("Unknown direction")
+}
+
+switch direction {
+case .north:
+  print("We are heading north!")
+case .south:
+  print("We are heading south!")
+case .east:
+  print("We are heading east!")
+case .west:
+  print("We are heading west!")
+}
+
