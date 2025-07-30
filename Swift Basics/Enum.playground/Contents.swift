bb1232b83bff18f4ab0264ee1dbd283e34b70b7d
배정만 <jungman82@gmail.com>
2025-07-30 10:40:09 +0900

    미디어 열거형 추가 및 관련 정보 출력 함수 구현

--- Swift Basics/Enum.playground/Contents.swift
+++ Swift Basics/Enum.playground/Contents.swift
@@ -33,3 +33,40 @@ case .west:
   print("We are heading west!")
 }
 
+// 각 케이스가 고유한 연관 값을 가질 수 있습니다.
+enum Media {
+  case book(title: String, author: String, year: Int)
+  case movie(title: String, director: String, runningTime: Int)
+  case music(title: String, artist: String, album: String)
+}
+
+let myBook: Media = .book(title: "스위프트 프로그래밍", author: "야곰", year: 2022)
+let myMovie = Media.movie(title: "올드보이", director: "박찬욱", runningTime: 120)
+let myMusic = Media.music(title: "Hype Boy", artist: "NewJeans", album: "New Jeans")
+
+func printMediaInfo(for media: Media) {
+  switch media {
+  case .book(let title, let author, let year):
+    print("📚 책 정보")
+    print("   - 제목: \(title)")
+    print("   - 저자: \(author)")
+    print("   - 출판년도: \(year)년")
+
+  case .movie(let title, let director, let time):
+    print("🎬 영화 정보")
+    print("   - 제목: \(title)")
+    print("   - 감독: \(director)")
+    print("   - 상영시간: \(time)분")
+
+    // 'let'을 괄호 밖으로 빼서 한번에 바인딩할 수도 있습니다.
+  case let .music(title, artist, album):
+    print("🎵 음악 정보")
+    print("   - 제목: \(title)")
+    print("   - 아티스트: \(artist)")
+    print("   - 앨범: \(album)")
+  }
+}
+
+printMediaInfo(for: myBook)
+printMediaInfo(for: myMovie)
+printMediaInfo(for: myMusic)
