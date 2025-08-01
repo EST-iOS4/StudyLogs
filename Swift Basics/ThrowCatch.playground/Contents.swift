46cdf8aa09a5bd0be710a100167266da9e525de5
배정만 <jungman82@gmail.com>
2025-08-01 14:22:55 +0900

    defer: 함수 완료 실행 구문

--- Swift Basics/ThrowCatch.playground/Contents.swift
+++ Swift Basics/ThrowCatch.playground/Contents.swift
@@ -6,6 +6,7 @@ enum MealState {
   case cook // 요리
   case plateUp // 플레이팅
   case serve // 서빙
+  case complete // 완료
 }
 
 enum MealError: Error {
@@ -17,27 +18,46 @@ class Meal {
 
   // 단계별로 상태를 변경하는 메서드
   func change(to newState: MealState) throws {
+    print("change 함수 진입")
+    defer {
+      state = .complete
+      print("defer 상태 변경 완료")
+    }
     switch (state, newState) {
     case (.initial, .buyIngredients),
       (.buyIngredients, .prepareIngredients),
       (.prepareIngredients, .cook),
       (.cook, .plateUp),
       (.plateUp, .serve):
+      print("상태 변경 진입")
       state = newState
     default:
+      print("예외 상황 진입")
       throw MealError.canOnlyMoveToAppropriateState // 순서가 잘못되었다는 오류
     }
   }
 }
 
 let dinner = Meal()
-print("Current dinner state #1: \(dinner.state)") // Current dinner state: initial
 //dinner.state = .buyIngredients 직접 접근은 오류
 do {
+  print("----------")
   try dinner.change(to: .buyIngredients) // 재료 구매 단계로 변경
-  print("Current dinner state #2: \(dinner.state)") // Current dinner state: initial
-  try dinner.change(to: .serve)
+  print("----------")
+  try dinner.change(to: .prepareIngredients) // 재료 준비 단계로 변경
+  print("----------")
+  try dinner.change(to: .cook) // 요리 단계로 변경
+  print("----------")
+  try dinner.change(to: .plateUp) // 플레이팅 단계로 변경
+  print("----------")
+  try dinner.change(to: .serve) // 서빙 단계로 변경
+  print("저녁준비가 끝났습니다.")
 } catch let error {
   print("Error changing state: \(error)")
 }
+
+// try? 는 오류가 발생해도 무시하고 nil을 반환
+try? dinner.change(to: .initial) // 잘못된 상태 변경 시도
+
+print("현재 상태: \(dinner.state)") // 현재 상태 출력
 
