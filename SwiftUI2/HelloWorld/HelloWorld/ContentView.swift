41fabd8d8dc6f4a6d851c4b990aed0c01fbf5068
배정만 <jungman82@gmail.com>
2025-08-04 11:38:00 +0900

    stack의 각종 정렬값 실습: VStack 정렬 변경 및 여러 HStack 추가로 레이아웃 개선

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -12,10 +12,78 @@ struct ContentView: View {
 
   var body: some View {
     // alignment: leading, center, trailing
-    VStack(alignment: .center, spacing: 10) {
+    VStack(alignment: .leading, spacing: 10) {
       Text("첫 번째 텍스트")
       Text("텍스트 #2")
       Text("#3")
+
+      // alignment: top, center, bottom
+      HStack(alignment: .bottom) {
+        Rectangle()
+          .foregroundStyle(.red)
+          .frame(width: 10, height: 10)
+        Rectangle()
+          .foregroundStyle(.blue)
+          .frame(width: 30, height: 30)
+        Rectangle()
+          .foregroundStyle(.orange)
+          .frame(width: 100, height: 100)
+
+      }
+
+      HStack {
+        ZStack(alignment: .bottomLeading) {
+          Rectangle()
+            .foregroundStyle(.orange)
+            .frame(width: 100, height: 100)
+          Rectangle()
+            .foregroundStyle(.blue)
+            .frame(width: 30, height: 30)
+          Rectangle()
+            .foregroundStyle(.red)
+            .frame(width: 10, height: 10)
+
+        }
+        ZStack(alignment: .topTrailing) {
+          Rectangle()
+            .foregroundStyle(.orange)
+            .frame(width: 100, height: 100)
+          Rectangle()
+            .foregroundStyle(.blue)
+            .frame(width: 30, height: 30)
+          Rectangle()
+            .foregroundStyle(.red)
+            .frame(width: 10, height: 10)
+
+        }
+      }
+      HStack {
+        ZStack(alignment: .center) {
+          Rectangle()
+            .foregroundStyle(.orange)
+            .frame(width: 100, height: 100)
+          Rectangle()
+            .foregroundStyle(.blue)
+            .frame(width: 30, height: 30)
+          Rectangle()
+            .foregroundStyle(.red)
+            .frame(width: 10, height: 10)
+
+        }
+        ZStack(alignment: .trailing) {
+          Rectangle()
+            .foregroundStyle(.orange)
+            .frame(width: 100, height: 100)
+          Rectangle()
+            .foregroundStyle(.blue)
+            .frame(width: 30, height: 30)
+          Rectangle()
+            .foregroundStyle(.red)
+            .frame(width: 10, height: 10)
+
+        }
+      }
+
     }
   }
 }
