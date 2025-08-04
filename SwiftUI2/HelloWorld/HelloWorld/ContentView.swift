69a8f6da4437fe1229033e57c0608b9564893d96
배정만 <jungman82@gmail.com>
2025-08-04 15:32:04 +0900

    이미지 스타일 변경, 그림자,  크기 및 모양 변경, Ellipse 색상 및 크기 조정

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -11,28 +11,38 @@ struct ContentView: View {
   let myString = 43
 
   var body: some View {
-    Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-      .lineLimit(2)
-      .truncationMode(.tail)
-      .font(.custom("Courier", size: 24))
-      .fontWeight(.semibold)
-      .foregroundStyle(.red)
-      .padding()
-    Label("Text", systemImage: "car.fill")
-      .labelStyle(TrailingIconLabelStyle())
-      .font(.custom("Courier", size: 24))
+    VStack {
+      Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+        .lineLimit(2)
+        .truncationMode(.tail)
+        .font(.custom("Courier", size: 24))
+        .fontWeight(.semibold)
+        .foregroundStyle(.red)
+        .padding()
+      Label("Text", systemImage: "car.fill")
+        .labelStyle(TrailingIconLabelStyle())
+        .font(.custom("Courier", size: 24))
 
-    Label {
-      Text("Modifiers")
-        .font(.title)
-    } icon: {
-      Image("flag")
-        .resizable()
-        .aspectRatio(contentMode: .fill)
-        .frame(width: 120, height: 80)
-        .clipped()
-        .border(Color.blue, width: 2)
+      Label {
+        Text("Modifiers")
+          .font(.title)
+      } icon: {
+        Image("flag")
+          .resizable()
+          .aspectRatio(contentMode: .fill)
+          .frame(width: 100, height: 100)
+          .clipShape(Circle())
+          .shadow(color: .red, radius: 10)
+          .opacity(0.5)
+
+      }
+
+      Ellipse()
+        .fill(Color(hue: 1.7, saturation: 2.9, brightness: 0.58))
+        .frame(width: 100, height: 200)
     }
+    .padding()
+    .background(.gray)
   }
 }
 
