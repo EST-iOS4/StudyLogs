af4dae1b8b86c40ae70930003359104b0fd05b9a
배정만 <jungman82@gmail.com>
2025-08-04 14:12:53 +0900

    Text

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -8,83 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var value = 0
+  let myString = 43
 
   var body: some View {
-    // alignment: leading, center, trailing
-    VStack(alignment: .leading, spacing: 10) {
-      Text("첫 번째 텍스트")
-      Text("텍스트 #2")
-      Text("#3")
-
-      // alignment: top, center, bottom
-      HStack(alignment: .bottom) {
-        Rectangle()
-          .foregroundStyle(.red)
-          .frame(width: 10, height: 10)
-        Rectangle()
-          .foregroundStyle(.blue)
-          .frame(width: 30, height: 30)
-        Rectangle()
-          .foregroundStyle(.orange)
-          .frame(width: 100, height: 100)
-
-      }
-
-      HStack {
-        ZStack(alignment: .bottomLeading) {
-          Rectangle()
-            .foregroundStyle(.orange)
-            .frame(width: 100, height: 100)
-          Rectangle()
-            .foregroundStyle(.blue)
-            .frame(width: 30, height: 30)
-          Rectangle()
-            .foregroundStyle(.red)
-            .frame(width: 10, height: 10)
-
-        }
-        ZStack(alignment: .topTrailing) {
-          Rectangle()
-            .foregroundStyle(.orange)
-            .frame(width: 100, height: 100)
-          Rectangle()
-            .foregroundStyle(.blue)
-            .frame(width: 30, height: 30)
-          Rectangle()
-            .foregroundStyle(.red)
-            .frame(width: 10, height: 10)
-
-        }
-      }
-      HStack {
-        ZStack(alignment: .center) {
-          Rectangle()
-            .foregroundStyle(.orange)
-            .frame(width: 100, height: 100)
-          Rectangle()
-            .foregroundStyle(.blue)
-            .frame(width: 30, height: 30)
-          Rectangle()
-            .foregroundStyle(.red)
-            .frame(width: 10, height: 10)
-
-        }
-        ZStack(alignment: .trailing) {
-          Rectangle()
-            .foregroundStyle(.orange)
-            .frame(width: 100, height: 100)
-          Rectangle()
-            .foregroundStyle(.blue)
-            .frame(width: 30, height: 30)
-          Rectangle()
-            .foregroundStyle(.red)
-            .frame(width: 10, height: 10)
-
-        }
-      }
-
-    }
+    Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
+      .lineLimit(2)
+      .truncationMode(.tail)
+      .padding()
   }
 }
 
