95605e6cbe720ccaea4c2c246956bce12d1e96a0
배정만 <jungman82@gmail.com>
2025-08-05 10:47:21 +0900

    로그인 화면 개선: 비밀번호 확인 필드 추가 및 입력 검증 로직 구현

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
   @State private var username: String = ""
   @State private var password: String = ""
-
+  @State private var confirmPassword: String = ""
+  
   var body: some View {
     VStack {
       Text("로그인")
@@ -18,11 +19,18 @@ struct ContentView: View {
         .padding()
       TextField("사용자 이름을 입력하세요", text: $username)
         .padding()
-        .border(.black, width: 1)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2)) 
       SecureField("비밀번호를 입력하세요", text: $password)
         .padding()
-        .border(.black, width: 1)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
+      SecureField("비밀번호 확인 ", text: $confirmPassword)
+        .padding()
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
       Button(action: {
+        if password != confirmPassword {
+          print("비밀번호가 일치하지 않습니다.")
+          return
+        }
         print("사용자 이름: \(username), 비밀번호: \(password)")
         // API 호출
       }) {
