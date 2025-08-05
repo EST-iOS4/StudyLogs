7b826456a6256425f20f27395a37435ae1889f3a
배정만 <jungman82@gmail.com>
2025-08-05 11:43:55 +0900

    <실습> Calculator 프로젝트 초기 설정 및 기본 UI 구성 추가

---
+++ SwiftUI2/Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Jungman Bae on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI2/Calculator/Calculator/ContentView.swift
@@ -0,0 +1,136 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Jungman Bae on 8/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State private var formulaString = ""
+  @State private var resultString = ""
+
+  var body: some View {
+    VStack(alignment: .trailing) {
+      Text(formulaString)
+        .foregroundStyle(.gray)
+      Text(resultString)
+        .font(.custom("Helvetica Neue", size: 72))
+      HStack {
+        Button {} label: {
+          Text("AC")
+        }
+          .buttonStyle(.bordered)
+        Button {} label: {
+          Text("+/-")
+        }
+          .buttonStyle(.bordered)
+        Button {} label: {
+          Text("%")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("÷")
+        }
+        .buttonStyle(.bordered)
+
+      }
+      HStack {
+        Button {
+          appendToFormula("7")
+        } label: {
+          Text("7")
+        }
+        .buttonStyle(.bordered)
+        Button {
+
+        } label: {
+          Text("8")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("9")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("×")
+        }
+        .buttonStyle(.bordered)
+      }
+      HStack {
+        Button {} label: {
+          Text("4")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("5")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("6")
+        }
+        .buttonStyle(.bordered)
+        Button {
+          appendToFormula("−")
+        } label: {
+          Text("−")
+        }
+        .buttonStyle(.bordered)
+      }
+      HStack {
+        Button {} label: {
+          Text("1")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("2")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("3")
+        }
+        .buttonStyle(.bordered)
+        Button {
+          appendToFormula("+")
+        } label: {
+          Text("+")
+        }
+        .buttonStyle(.bordered)
+      }
+      HStack {
+        Button {} label: {
+          Text("📱")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("0")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text(".")
+        }
+        .buttonStyle(.bordered)
+        Button {} label: {
+          Text("=")
+        }
+        .buttonStyle(.bordered)
+      }
+    }
+    .font(.largeTitle)
+    .padding()
+  }
+
+  private func appendToFormula(_ value: String) {
+    // Check if the last character is an operator
+    if let lastCharacter = formulaString.last, "+−×÷".contains(lastCharacter) && "+−×÷".contains(value) {
+      // If the last character is an operator, replace it with the new value
+      formulaString.removeLast()
+    }
+    formulaString += value
+  }
+}
+
+#Preview {
+  ContentView()
+}
