0621c4000c781dcd399e35724f9034affac9214f
배정만 <jungman82@gmail.com>
2025-08-05 11:14:28 +0900

    Label 과 HStack Text, Image 조합 비교

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var email: String = ""
   @State private var username: String = ""
   @State private var password: String = ""
   @State private var confirmPassword: String = ""
-  
+
   var body: some View {
     VStack {
-      Text("로그인")
+      Label("회원가입", systemImage: "person.crop.circle.fill.badge.plus")
         .font(.largeTitle)
         .padding()
+
+      HStack {
+        Image("flag")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .frame(width: 50, height: 50)
+          .clipShape(Circle())
+        Text("회원가입")
+          .font(.largeTitle)
+      }
+      .font(.largeTitle)
+
+
+
+      TextField("이메일", text: $email)
+        .keyboardType(.emailAddress) // 이메일 입력을 위한 키보드 타입
+        .submitLabel(.next) // 다음 버튼을 표시
+        .autocapitalization(.none) // 텍스트 필드 자동 대문자 변환을 방지
+        .padding()
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
+
       TextField("사용자 이름을 입력하세요", text: $username)
         .padding()
-        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2)) 
+        .autocapitalization(.none) // 텍스트 필드 자동 대문자 변환을 방지
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
       SecureField("비밀번호를 입력하세요", text: $password)
         .padding()
         .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
