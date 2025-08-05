87d254c12f0543dfd1ea9e588dd33d07bd1bb005
배정만 <jungman82@gmail.com>
2025-08-05 15:47:33 +0900

    UI 개선: 회원가입 화면을 색상 선택기와 날짜 선택기로 변경

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -8,62 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var email: String = ""
-  @State private var username: String = ""
-  @State private var password: String = ""
-  @State private var confirmPassword: String = ""
+  @State private var selectedColor: Color = .blue
+  @State private var date: Date = Date()
 
   var body: some View {
     VStack {
-      Label("회원가입", systemImage: "person.crop.circle.fill.badge.plus")
-        .font(.largeTitle)
-        .padding()
-
-      HStack {
-        Image("flag")
-          .resizable()
-          .aspectRatio(contentMode: .fit)
-          .frame(width: 50, height: 50)
-          .clipShape(Circle())
-        Text("회원가입")
-          .font(.largeTitle)
+      ColorPicker(selection: $selectedColor) {
+        Text("Select a color")
+          .foregroundColor(.primary)
       }
-      .font(.largeTitle)
 
-
-
-      TextField("이메일", text: $email)
-        .keyboardType(.emailAddress) // 이메일 입력을 위한 키보드 타입
-        .submitLabel(.next) // 다음 버튼을 표시
-        .autocapitalization(.none) // 텍스트 필드 자동 대문자 변환을 방지
-        .padding()
-        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
-
-      TextField("사용자 이름을 입력하세요", text: $username)
-        .padding()
-        .autocapitalization(.none) // 텍스트 필드 자동 대문자 변환을 방지
-        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
-      SecureField("비밀번호를 입력하세요", text: $password)
-        .padding()
-        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
-      SecureField("비밀번호 확인 ", text: $confirmPassword)
-        .padding()
-        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray, lineWidth: 2))
-      Button(action: {
-        if password != confirmPassword {
-          print("비밀번호가 일치하지 않습니다.")
-          return
-        }
-        print("사용자 이름: \(username), 비밀번호: \(password)")
-        // API 호출
-      }) {
-        Text("로그인")
-          .frame(minWidth: 0, maxWidth: .infinity)
-          .padding()
-          .background(Color.blue)
-          .foregroundColor(.white)
-          .cornerRadius(8)
-      }
+      DatePicker(
+        "Select a date",
+        selection: $date,
+        displayedComponents: [.date, .hourAndMinute]
+      )
+      // .wheel, .compact, .graphical 스타일을 사용할 수 있습니다.
+      .datePickerStyle(.compact)
     }
     .padding()
   }
