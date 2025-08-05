a6dab7d3c848ba2f79ec10bb7cd986b751c01062
배정만 <jungman82@gmail.com>
2025-08-05 09:15:56 +0900

    버튼 실습

--- SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI2/HelloWorld/HelloWorld/ContentView.swift
@@ -11,47 +11,14 @@ struct ContentView: View {
   let myString = 43
 
   var body: some View {
-    VStack {
-      Text("This is my age \(myString). Since I am retired, I am now eligible for a pension and Social Security so I can spend the rest of my life relaxing and enjoying life without having to work for an income anymore.")
-        .lineLimit(2)
-        .truncationMode(.tail)
-        .font(.custom("Courier", size: 24))
-        .fontWeight(.semibold)
-        .foregroundStyle(.red)
+    Button {
+      print("click!") // 실행 코드
+    } label: {
+      Text("Click here")
+        .font(.largeTitle)
+        .foregroundStyle(.green)
         .padding()
-      Label("Text", systemImage: "car.fill")
-        .labelStyle(TrailingIconLabelStyle())
-        .font(.custom("Courier", size: 24))
-
-      Label {
-        Text("Modifiers")
-          .font(.title)
-      } icon: {
-        Image("flag")
-          .resizable()
-          .aspectRatio(contentMode: .fill)
-          .frame(width: 100, height: 100)
-          .clipShape(Circle())
-          .shadow(color: .red, radius: 10)
-          .opacity(0.5)
-
-      }
-
-      Ellipse()
-        .fill(Color(hue: 1.7, saturation: 2.9, brightness: 0.58))
-        .frame(width: 100, height: 200)
-    }
-    .padding()
-    .background(.gray)
-  }
-}
-
-// 커스텀 LabelStyle: 아이콘을 텍스트 뒤에 배치
-struct TrailingIconLabelStyle: LabelStyle {
-  func makeBody(configuration: Configuration) -> some View {
-    HStack {
-      configuration.title
-      configuration.icon
+        .border(.red, width: 6)
     }
   }
 }
