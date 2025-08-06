973098cc53e3cb6332feb44f6ad2f2cab57cb30d
배정만 <jungman82@gmail.com>
2025-08-06 09:43:41 +0900

    초기 프로젝트 파일 및 기본 UI 구성 추가: ContentView 및 HelloWorld2App 생성

---
+++ SwiftUI2/HelloWorld2/HelloWorld2/HelloWorld2App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld2App.swift
+//  HelloWorld2
+//
+//  Created by Jungman Bae on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
