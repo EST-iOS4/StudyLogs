b67bf2f6200bbb8e668d31e83617901606490ece
배정만 <jungman82@gmail.com>
2025-08-07 09:26:44 +0900

    프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 UI 구성 추가

---
+++ SwiftUI2/ListNavigation/ListNavigation/ListNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavigationApp.swift
+//  ListNavigation
+//
+//  Created by Jungman Bae on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ListNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
