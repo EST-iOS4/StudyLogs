ffbf4263766d4bdde226546ef85615657c12d0cb
배정만 <jungman82@gmail.com>
2025-08-07 15:09:24 +0900

    Grid 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 뷰 추가, 애플리케이션 아이콘 및 색상 자산 구성

---
+++ SwiftUI2/Grid/Grid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grid
+//
+//  Created by Jungman Bae on 8/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ SwiftUI2/Grid/Grid/GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridApp.swift
+//  Grid
+//
+//  Created by Jungman Bae on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
