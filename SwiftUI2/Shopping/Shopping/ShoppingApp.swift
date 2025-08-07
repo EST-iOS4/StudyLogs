dd9cd3c5024e17e45ac049e7e31ea2c41816a015
배정만 <jungman82@gmail.com>
2025-08-07 13:29:21 +0900

    Shopping 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 뷰 추가

---
+++ SwiftUI2/Shopping/Shopping/ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingApp.swift
+//  Shopping
+//
+//  Created by Jungman Bae on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
