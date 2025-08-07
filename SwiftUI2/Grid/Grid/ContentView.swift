65d27a8cd4fb1e07cdf2acea1e5bfa7fa200b5ac
배정만 <jungman82@gmail.com>
2025-08-07 15:18:42 +0900

    그리드 레이아웃 추가: 수평 및 수직 스크롤 뷰에 LazyHGrid 및 LazyVGrid 구현

--- SwiftUI2/Grid/Grid/ContentView.swift
+++ SwiftUI2/Grid/Grid/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+  let columns = [
+    GridItem(.fixed(100)),
+    GridItem(.fixed(100)),
+    GridItem(.fixed(100))
+  ]
+
+  let adaptiveColumns = [
+    GridItem(.adaptive(minimum: 100))
+  ]
+
+  let mixedColumns = [
+    GridItem(.fixed(100)),
+    GridItem(.flexible()),
+    GridItem(.adaptive(minimum: 50))
+  ]
+
   var body: some View {
-    VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+    ScrollView(.horizontal) {
+      LazyHGrid(rows: columns, spacing: 20) {
+        ForEach(0..<30) { index in
+          Rectangle()
+            .fill(Color.blue)
+            .frame(width: 100)
+            .overlay(
+              Text("\(index)")
+                .foregroundColor(.white)
+                .font(.headline)
+            )
+        }
+      }
     }
-    .padding()
+    ScrollView {
+      LazyVGrid(columns: adaptiveColumns, spacing: 20) {
+        ForEach(0..<30) { index in
+          Rectangle()
+            .fill(Color.red)
+            .frame(height: 100)
+            .overlay(
+              Text("\(index)")
+                .foregroundColor(.white)
+                .font(.headline)
+            )
+        }
+      }
+
+      LazyVGrid(columns: mixedColumns, spacing: 20) {
+        ForEach(0..<30) { index in
+          Rectangle()
+            .fill(Color.green)
+            .frame(height: 100)
+            .overlay(
+              Text("\(index)")
+                .foregroundColor(.white)
+                .font(.headline)
+            )
+        }
+      }
+
+    }
   }
 }
 
