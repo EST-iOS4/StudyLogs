ff9f654e2a4e41649c083483da6717ebdd534b14
배정만 <jungman82@gmail.com>
2025-08-07 14:29:27 +0900

    Animation 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 뷰 추가, 애플리케이션 아이콘 및 색상 자산 구성

---
+++ SwiftUI2/Animation/Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Jungman Bae on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
