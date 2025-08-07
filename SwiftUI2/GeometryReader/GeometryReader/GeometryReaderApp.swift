79fdbcfec4ebb0f0519c6d2ab92d0c86046ea4b7
배정만 <jungman82@gmail.com>
2025-08-07 15:36:02 +0900

    GeometryReader 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 뷰 추가, 애플리케이션 아이콘 및 색상 자산 구성

---
+++ SwiftUI2/GeometryReader/GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Jungman Bae on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
