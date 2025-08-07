24e4fa450014df7afeb75f3bafcdf8f5ff8dd51c
배정만 <jungman82@gmail.com>
2025-08-07 09:37:57 +0900

    List, Section 목록 뷰 가족 및 친구 목록을 표시하는 리스트 추가

--- SwiftUI2/ListNavigation/ListNavigation/ContentView.swift
+++ SwiftUI2/ListNavigation/ListNavigation/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  let familyMembers = ["아빠", "엄마", "형", "누나", "동생"]
+  let friends = ["친구1", "친구2", "친구3", "친구4"]
+
+  var body: some View {
+    List {
+      Section("가족") {
+        ForEach(familyMembers, id: \.self) { member in
+          Text(member)
         }
-        .padding()
+      }
+
+      Section("친구") {
+        ForEach(friends, id: \.self) { friend in
+          Text(friend)
+        }
+      }
+
     }
+    //          .listStyle(.plain)
+    //          .listStyle(.inset)
+    .listStyle(.grouped)
+    //      .listStyle(.insetGrouped)
+    //    .listStyle(.sidebar)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
