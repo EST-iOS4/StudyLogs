70cfd86722e5267a184319b53ba41fb2be936dc5
배정만 <jungman82@gmail.com>
2025-08-08 09:24:54 +0900

    Form 실습:  프로젝트 생성

---
+++ SwiftUI2/AdvancedForm/AdvancedForm/AdvancedFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedFormApp.swift
+//  AdvancedForm
+//
+//  Created by Jungman Bae on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
