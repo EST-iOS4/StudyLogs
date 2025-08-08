9f5c3897098011cfc3a9042ce95009cb6eb9b837
배정만 <jungman82@gmail.com>
2025-08-08 14:31:45 +0900

    SwiftData 를 이용한 TodoApp 프로젝트 초기 설정: Xcode 프로젝트 파일, 앱 아이콘 및 색상 자산 추가, ContentView 및 Item 모델 구현

---
+++ SwiftUI2/TodoApp/TodoApp/TodoApp.swift
@@ -0,0 +1,32 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Jungman Bae on 8/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TodoApp: App {
+  var sharedModelContainer: ModelContainer = {
+    let schema = Schema([
+      Item.self,
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+    
+    do {
+      return try ModelContainer(for: schema, configurations: [modelConfiguration])
+    } catch {
+      fatalError("Could not create ModelContainer: \(error)")
+    }
+  }()
+  
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+    .modelContainer(sharedModelContainer)
+  }
+}
