3c209235fc63ab83d1d292bddf416c288515a3d1
배정만 <jungman82@gmail.com>
2025-08-08 11:40:05 +0900

    Async/Await 프로젝트 초기 설정: Xcode 프로젝트 파일, 앱 아이콘 및 색상 자산 추가, ContentView 구현

---
+++ SwiftUI2/AsyncAwait/AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by Jungman Bae on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
