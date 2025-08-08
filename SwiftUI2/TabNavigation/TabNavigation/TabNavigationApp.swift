2ffa37348bd2732d164c2c2cafe0b11592714d1e
배정만 <jungman82@gmail.com>
2025-08-08 10:22:25 +0900

    Tab, Navigation 프로젝트 초기 설정: Xcode 프로젝트 파일, 기본 뷰 및 자산 구성

---
+++ SwiftUI2/TabNavigation/TabNavigation/TabNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabNavigationApp.swift
+//  TabNavigation
+//
+//  Created by Jungman Bae on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TabNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
