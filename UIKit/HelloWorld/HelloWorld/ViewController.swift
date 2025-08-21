b6bea696cfda37328827d2f424557da546637a75
배정만 <jungman82@gmail.com>
2025-08-21 11:01:48 +0900

    Main.storyboard 및 ViewController 수정: 버튼 추가 및 IBOutlet 연결, 레이아웃 조정

--- UIKit/HelloWorld/HelloWorld/ViewController.swift
+++ UIKit/HelloWorld/HelloWorld/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var greetingLabel: UILabel!
+  @IBOutlet weak var nameTextField: UITextField!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    greetingLabel.text = "UIKit에 오신 것을 환영합니다!"
   }
 
+  @IBAction func sayHelloButtonTapped(_ sender: Any) {
+    let name = nameTextField.text ?? ""
+    if name.isEmpty {
+      greetingLabel.text = "이름을 입력해주세요."
+    } else {
+      greetingLabel.text = "안녕하세요, \(name)님!"
+    }
+  }
 
 }
-
