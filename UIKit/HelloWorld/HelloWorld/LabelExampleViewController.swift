6a99302f059ac60d073e37ffd0d0c8247313f5a9
배정만 <jungman82@gmail.com>
2025-08-21 13:18:26 +0900

    LabelExampleViewController:  레이블 실습

--- UIKit/HelloWorld/HelloWorld/LabelExampleViewController.swift
+++ UIKit/HelloWorld/HelloWorld/LabelExampleViewController.swift
@@ -8,22 +8,29 @@
 import UIKit
 
 class LabelExampleViewController: UIViewController {
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var descriptionLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-    
+    titleLabel.text = "UILabel 마스터하기"
+    descriptionLabel.text = """
+      Hello, UIKit! UILabel은 iOS 앱에서 텍스트를 표시하는 기본적인 방법입니다.
+      Discriminating between different text styles, colors, and alignments is essential for creating a visually appealing interface.
+      """
 
-    /*
-    // MARK: - Navigation
+    titleLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+    descriptionLabel.font = UIFont(name: "Helvetica", size: 16)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    titleLabel.textColor = .systemBlue
+    descriptionLabel.textColor = .darkGray
+
+    titleLabel.textAlignment = .center
+    descriptionLabel.textAlignment = .left
+
+    descriptionLabel.numberOfLines = 0
+    descriptionLabel.lineBreakMode = .byWordWrapping
+  }
 
 }
