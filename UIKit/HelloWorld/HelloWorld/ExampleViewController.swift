d1b5a42837ccf1e31bdae618423cb9d4612181cc
배정만 <jungman82@gmail.com>
2025-08-21 15:34:17 +0900

    배경 및 프로필 이미지 추가: 이미지 뷰 설정 및 스타일링 구현

--- UIKit/HelloWorld/HelloWorld/ExampleViewController.swift
+++ UIKit/HelloWorld/HelloWorld/ExampleViewController.swift
@@ -8,8 +8,22 @@
 import UIKit
 
 class ExampleViewController: UIViewController {
+  @IBOutlet weak var profileImageView: UIImageView!
+  @IBOutlet weak var backgroundImageView: UIImageView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+
+    profileImageView.image = UIImage(named: "profile")
+
+    profileImageView.contentMode = .scaleAspectFill
+
+    profileImageView.layer.cornerRadius = profileImageView.frame.width / 2
+    profileImageView.clipsToBounds = true
+
+    profileImageView.layer.borderWidth = 3
+    profileImageView.layer.borderColor = UIColor.blue.cgColor
+
 
   }
 }
