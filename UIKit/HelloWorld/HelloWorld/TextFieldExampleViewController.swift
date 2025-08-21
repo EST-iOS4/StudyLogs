8b1aff4be0996eeff8f516aaaa1eb93037a2a2fc
배정만 <jungman82@gmail.com>
2025-08-21 14:28:53 +0900

    TextFieldExampleViewController 추가: 이메일 및 비밀번호 입력 필드 구현

---
+++ UIKit/HelloWorld/HelloWorld/TextFieldExampleViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TextFieldExampleViewController.swift
+//  HelloWorld
+//
+//  Created by Jungman Bae on 8/21/25.
+//
+
+import UIKit
+
+class TextFieldExampleViewController: UIViewController {
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+  }
+}
