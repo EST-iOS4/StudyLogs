104537421f32ecc91cba9c7024e2496de07c11e6
배정만 <jungman82@gmail.com>
2025-08-22 13:03:42 +0900

    TableView 편집 기능 추가, 아이템 배열을 var로 변경

--- UIKit/TableView/TableView/ViewController.swift
+++ UIKit/TableView/TableView/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
 
-  let items = ["Apple #1", "Banana #2", "Cherry #3", "Date #4",
+  var items = ["Apple #1", "Banana #2", "Cherry #3", "Date #4",
                "Elderberry #5", "Fig #6", "Grape #7", "Honeydew #8"]
 
   override func viewDidLoad() {
@@ -19,7 +19,15 @@ class ViewController: UIViewController {
     tableView.delegate = self
     tableView.dataSource = self
 
-    tableView.register(CustomCell.self, forCellReuseIdentifier: "Cell")
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "편집", style: .plain,
+                                                        target: self, action: #selector(editButtonTapped))
+  }
+
+  @objc func editButtonTapped() {
+    tableView.isEditing.toggle()
+    navigationItem.rightBarButtonItem?.title = tableView.isEditing ? "완료" : "편집"
   }
 }
 
@@ -29,9 +37,38 @@ extension ViewController: UITableViewDataSource {
     return items.count
   }
 
+  func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+
+  func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    moveRowAt sourceIndexPath: IndexPath,
+    to destinationIndexPath: IndexPath
+  ) {
+    let moveItem = items.remove(at: sourceIndexPath.row)
+    items.insert(moveItem, at: destinationIndexPath.row)
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    commit editingStyle: UITableViewCell.EditingStyle,
+    forRowAt indexPath: IndexPath
+  ) {
+    if editingStyle == .delete {
+      items.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    }
+  }
+
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomCell
-    cell.configure(with: items[indexPath.row])
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//    cell.configure(with: items[indexPath.row])
+    cell.textLabel?.text = items[indexPath.row]
     return cell
   }
 }
