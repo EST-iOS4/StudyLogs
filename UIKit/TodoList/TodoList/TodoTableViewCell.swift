f3c10eb2cb201645387f9960a8896305a84e2a74
배정만 <jungman82@gmail.com>
2025-08-25 10:29:28 +0900

    TodoTableViewCell 및 ViewController 업데이트: 완료 토글 기능 추가 및 코드 정리

--- UIKit/TodoList/TodoList/TodoTableViewCell.swift
+++ UIKit/TodoList/TodoList/TodoTableViewCell.swift
@@ -13,15 +13,36 @@ class TodoTableViewCell: UITableViewCell {
   @IBOutlet weak var titleLabel: UILabel!
   @IBOutlet weak var priorityView: UIView!
 
+  var onToggleComplete: (() -> Void)?
+
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
+    priorityView.layer.cornerRadius = 4
+    checkButton
+      .addTarget(self, action: #selector(toggleComplete), for: .touchUpInside)
   }
 
-  override func setSelected(_ selected: Bool, animated: Bool) {
-    super.setSelected(selected, animated: animated)
-
-    // Configure the view for the selected state
+  func configure(with todo: TodoItem) {
+    titleLabel.text = todo.title
+    titleLabel.textColor = todo.isCompleted ? .systemGray : .label
+    titleLabel.attributedText = todo.isCompleted ? strikeThrough(
+      text: todo.title
+    ) : NSAttributedString(string: todo.title)
   }
 
+  func strikeThrough(text: String) -> NSAttributedString {
+    let attributeString = NSMutableAttributedString(string: text)
+    attributeString
+      .addAttribute(
+        .strikethroughStyle,
+        value: NSUnderlineStyle.single.rawValue,
+        range: NSRange(location: 0, length: text.count)
+      )
+    return attributeString
+  }
+
+  @objc func toggleComplete() {
+    onToggleComplete?()
+  }
 }
