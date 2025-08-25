a444d22b23f4f44b1fb335ab06964cc4a16292e2
배정만 <jungman82@gmail.com>
2025-08-25 09:16:04 +0900

    TodoList 프로젝트 초기 설정

---
+++ UIKit/TodoList/TodoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Jungman Bae on 8/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
