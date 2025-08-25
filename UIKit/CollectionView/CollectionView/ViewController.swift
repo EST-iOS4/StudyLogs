fa38aeb16c20e3f5de6d49d1285332c49f7be800
배정만 <jungman82@gmail.com>
2025-08-25 13:25:26 +0900

    UICollectionView 추가: Main.storyboard 및 ViewController 업데이트

--- UIKit/CollectionView/CollectionView/ViewController.swift
+++ UIKit/CollectionView/CollectionView/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var collectionView: UICollectionView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    collectionView.dataSource = self
+    collectionView.delegate = self
   }
-
-
 }
 
+extension ViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 0
+  }
+
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    return UICollectionViewCell()
+  }
+}
+
+extension ViewController: UICollectionViewDelegate {
+  
+}
