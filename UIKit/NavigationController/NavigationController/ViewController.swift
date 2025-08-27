57d0debf63a9227f4b02a8e5f81361792b5cf839
배정만 <jungman82@gmail.com>
2025-08-27 10:05:06 +0900

    NavigationController 프로젝트 초기 설정: AppDelegate, SceneDelegate, ViewController 및 관련 리소스 추가

---
+++ UIKit/NavigationController/NavigationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by Jungman Bae on 8/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "메인 화면"
+    view.backgroundColor = .systemBackground  
+  }
+
+
+}
+
