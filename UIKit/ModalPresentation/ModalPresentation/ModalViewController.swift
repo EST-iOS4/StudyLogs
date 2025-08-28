0c68548fc81cfcdfa8a70bac1c25d347d6ebc137
배정만 <jungman82@gmail.com>
2025-08-28 10:03:47 +0900

    ModalViewController 추가 및 ViewController에서 모달 표시 버튼 설정

---
+++ UIKit/ModalPresentation/ModalPresentation/ModalViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ModalViewController.swift
+//  ModalPresentation
+//
+//  Created by Jungman Bae on 8/28/25.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+
+  func setupUI() {
+    view.backgroundColor = .systemBackground
+
+    let navBar = UINavigationBar()
+    navBar.translatesAutoresizingMaskIntoConstraints = false
+
+    let navItem = UINavigationItem(title: "새 항목 추가")
+    navItem.leftBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .cancel,
+      target: self,
+      action: #selector(cancelTapped)
+    )
+    navItem.rightBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .save,
+      target: self,
+      action: #selector(saveTapped)
+    )
+
+    navBar.setItems([navItem], animated: false)
+
+    view.addSubview(navBar)
+
+    NSLayoutConstraint.activate([
+      navBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      navBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      navBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+  }
+
+  @objc func cancelTapped() {
+    dismiss(animated: true)
+  }
+
+  @objc func saveTapped() {
+//    TODO: 데이터 전달 또는 저장
+    dismiss(animated: true)
+  }
+
+
+
+}
--- UIKit/ModalPresentation/ModalPresentation/ViewController.swift
+++ UIKit/ModalPresentation/ModalPresentation/ViewController.swift
@@ -13,8 +13,30 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     title = "Modal Presentaion"
     view.backgroundColor = .systemBackground
+
+    var config = UIButton.Configuration.filled()
+    config.title = "새 항목 추가"
+
+    let button = UIButton(configuration: config)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+
+    view.addSubview(button)
+
+    NSLayoutConstraint.activate([
+      button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      button.widthAnchor.constraint(equalToConstant: 150),
+      button.heightAnchor.constraint(equalToConstant: 50)
+    ])
+
   }
 
-
+  @objc func buttonTapped() {
+    let modalVC = ModalViewController()
+    modalVC.modalPresentationStyle = .fullScreen
+    modalVC.modalTransitionStyle = .coverVertical
+    present(modalVC, animated: true)
+  }
 }
 
