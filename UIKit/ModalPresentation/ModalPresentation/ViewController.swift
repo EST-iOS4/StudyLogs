93138667c4642fe163c91406b57e95793d2bc6a0
배정만 <jungman82@gmail.com>
2025-08-28 09:27:28 +0900

    ModalPresentation 프로젝트 초기 설정 및 기본 구조 추가

---
+++ UIKit/ModalPresentation/ModalPresentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModalPresentation
+//
+//  Created by Jungman Bae on 8/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Modal Presentaion"
+    view.backgroundColor = .systemBackground
+  }
+
+
+}
+
