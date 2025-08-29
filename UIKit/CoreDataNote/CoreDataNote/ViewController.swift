f7a9cdcb74b5e4fff0244c1f184b2052ea3faed2
배정만 <jungman82@gmail.com>
2025-08-29 13:21:32 +0900

    프로젝트 초기 설정: AppDelegate, SceneDelegate, ViewController 및 관련 리소스 파일 추가

---
+++ UIKit/CoreDataNote/CoreDataNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataNote
+//
+//  Created by Jungman Bae on 8/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .systemBackground
+  }
+
+
+}
+
