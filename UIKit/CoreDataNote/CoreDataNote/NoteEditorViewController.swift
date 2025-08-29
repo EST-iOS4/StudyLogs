f0bc55b6df7916565152bbbc112b90830094e890
배정만 <jungman82@gmail.com>
2025-08-29 15:33:54 +0900

    NoteEditorViewController 클래스 수정: 저장 및 취소 버튼에 UIAction 추가

--- UIKit/CoreDataNote/CoreDataNote/NoteEditorViewController.swift
+++ UIKit/CoreDataNote/CoreDataNote/NoteEditorViewController.swift
@@ -8,15 +8,34 @@
 import UIKit
 
 class NoteEditorViewController: UIViewController {
-  
+
   var note: Note?
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     title = ((note != nil) ? note?.title ?? "" : "New Note")
-    
-    navigationItem.rightBarButtonItem = UIBarButtonItem(systemItem: .save)
-    navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .cancel)
+
+    let saveAction = UIAction(title: "save", image: nil) { _ in
+      self.save()
+    }
+
+    // 1. 셀렉터를 이용한 버튼 이벤트, @objc 함수 시그니처 호출
+    navigationItem.rightBarButtonItem = UIBarButtonItem(
+      barButtonSystemItem: .save,
+      target: self,
+      action: #selector(save)
+    )
+
+    // 2. UIAction 을 이용한 버튼 이벤트
+    let cancelAction = UIAction(title: "", image: nil) { _ in
+      self.dismiss(animated: true)
+    }
+    navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .cancel, primaryAction: cancelAction)
   }
-  
+
+  @objc func save() {
+// note 저장 후 닫기
+    dismiss(animated: true)
+  }
+
 }
