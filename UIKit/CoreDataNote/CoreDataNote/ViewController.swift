c542ec1b78202e32a4ff66b6fca28b4ed867b0bc
배정만 <jungman82@gmail.com>
2025-08-29 14:48:49 +0900

    ViewController 클래스 수정: 노트 관리 및 검색 기능을 위한 테이블 뷰와 검색 바 추가

--- UIKit/CoreDataNote/CoreDataNote/ViewController.swift
+++ UIKit/CoreDataNote/CoreDataNote/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+  let noteManager = NoteManager()
+  var notes: [Note] = []
+
+  lazy var tableView: UITableView = {
+    let tableView = UITableView()
+    tableView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(tableView)
+    NSLayoutConstraint.activate([
+      tableView.topAnchor
+        .constraint(equalTo: self.searchBar.bottomAnchor),
+      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      tableView.bottomAnchor
+        .constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+    ])
+
+    return tableView
+  }()
+
+  lazy var searchBar: UISearchBar = {
+    let searchBar = UISearchBar()
+    searchBar.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(searchBar)
+
+    NSLayoutConstraint.activate([
+      searchBar.topAnchor
+        .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+    ])
+    return searchBar
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
+
+    tableView.dataSource = self
+    tableView.delegate = self
+
+    loadNotes()
   }
 
-
+  func loadNotes() {
+    notes = noteManager.searchNotes(keyword: searchBar.text ?? "")
+    tableView.reloadData()
+  }
 }
 
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
