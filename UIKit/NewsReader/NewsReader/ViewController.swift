05f7f40e13548e0b5a191139fd109e2d10749f45
배정만 <jungman82@gmail.com>
2025-09-01 13:12:04 +0900

    NewsReader 프로젝트 초기 설정 (Network 실습)

---
+++ UIKit/NewsReader/NewsReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsReader
+//
+//  Created by Jungman Bae on 9/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "News Reader"
+    view.backgroundColor = .systemBackground
+  }
+
+
+}
+
