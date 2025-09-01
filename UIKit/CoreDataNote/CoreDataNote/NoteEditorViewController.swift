1884a81d42e9a0e34e2a007a8813fdf759e8459d
배정만 <jungman82@gmail.com>
2025-09-01 09:33:23 +0900

    UIKit 뷰 구성: UIAction을 이용한 버튼 이벤트 추가 및 UI 구성 개선

--- UIKit/CoreDataNote/CoreDataNote/NoteEditorViewController.swift
+++ UIKit/CoreDataNote/CoreDataNote/NoteEditorViewController.swift
@@ -9,15 +9,19 @@ import UIKit
 
 class NoteEditorViewController: UIViewController {
 
+  let noteManager = NoteManager()
   var note: Note?
+
+  let titleText = UITextField()
+  let contentText = UITextView()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    title = ((note != nil) ? note?.title ?? "" : "New Note")
+    setupUI()
+  }
 
-    let saveAction = UIAction(title: "save", image: nil) { _ in
-      self.save()
-    }
+  func setupUI() {
+    title = ((note != nil) ? note?.title ?? "" : "New Note")
 
     // 1. 셀렉터를 이용한 버튼 이벤트, @objc 함수 시그니처 호출
     navigationItem.rightBarButtonItem = UIBarButtonItem(
@@ -31,10 +35,43 @@ class NoteEditorViewController: UIViewController {
       self.dismiss(animated: true)
     }
     navigationItem.leftBarButtonItem = UIBarButtonItem(systemItem: .cancel, primaryAction: cancelAction)
+
+    titleText.translatesAutoresizingMaskIntoConstraints = false
+    titleText.placeholder = "제목"
+    titleText.borderStyle = .roundedRect
+    view.addSubview(titleText)
+
+    contentText.translatesAutoresizingMaskIntoConstraints = false
+    contentText.backgroundColor = .systemGray
+    view.addSubview(contentText)
+
+    NSLayoutConstraint.activate([
+      // titleText
+      titleText.topAnchor
+        .constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+      titleText.leadingAnchor
+        .constraint(equalTo: view.leadingAnchor, constant: 20),
+      titleText.trailingAnchor
+        .constraint(equalTo: view.trailingAnchor, constant: -20),
+
+      // contentText
+      contentText.topAnchor
+        .constraint(equalTo: titleText.bottomAnchor, constant: 20),
+      contentText.leadingAnchor.constraint(equalTo: titleText.leadingAnchor),
+      contentText.trailingAnchor.constraint(equalTo: titleText.trailingAnchor),
+      contentText.heightAnchor.constraint(equalToConstant: 300)
+
+    ])
   }
 
   @objc func save() {
-// note 저장 후 닫기
+    if let title = titleText.text,
+       let content = contentText.text {
+      _ = noteManager
+        .createNote(title: title, content: content, image: nil)      
+    } else {
+      // TODO: Alert 오류 표시
+    }
     dismiss(animated: true)
   }
 
