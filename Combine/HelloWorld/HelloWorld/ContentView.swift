c6c38fac84d383f4fa277a4d05cf15472026a752
배정만 <jungman82@gmail.com>
2025-09-19 13:41:40 +0900

    ContentView 수정: 현재 시간을 표시하는 기능 추가 및 타이머 퍼블리셔 설정

--- Combine/HelloWorld/HelloWorld/ContentView.swift
+++ Combine/HelloWorld/HelloWorld/ContentView.swift
@@ -6,17 +6,27 @@
 //
 
 import SwiftUI
+internal import Combine
+
+let timerPublisher = Timer.publish(every: 1.0, on: .main, in: .common)
+  .autoconnect()
+
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+  @State private var currentTime = Date()
+
+  private let timeFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.timeStyle = .medium
+    return formatter
+  }()
+
+  var body: some View {
+    Text("현재 시간: \(currentTime, formatter: timeFormatter)")
+      .onReceive(timerPublisher) { time in
+        currentTime = time
+      }
+  }
 }
 
 #Preview {
