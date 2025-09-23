3f4bafaf087fad386859648470ac9e110a9e1ae5
배정만 <jungman82@gmail.com>
2025-09-23 11:08:54 +0900

    ErrorExample 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 뷰(ContentView) 생성, 앱 아이콘 및 색상 자산 추가

---
+++ Combine/ErrorExample/ErrorExample/ErrorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorExampleApp.swift
+//  ErrorExample
+//
+//  Created by Jungman Bae on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ErrorExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
