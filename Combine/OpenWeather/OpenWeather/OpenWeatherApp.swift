6aab8e6eeb99d21add965d9b1be1b04c03ab68d6
배정만 <jungman82@gmail.com>
2025-09-23 09:10:26 +0900

    프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 뷰 생성

---
+++ Combine/OpenWeather/OpenWeather/OpenWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherApp.swift
+//  OpenWeather
+//
+//  Created by Jungman Bae on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
