533b638183900ef3bf90a92b535d19dd4d8b43dd
배정만 <jungman82@gmail.com>
2025-09-24 11:47:32 +0900

    Firebase 설정 추가 및 Firestore 에뮬레이터 구성

--- Combine/Firebase/HelloWorld/HelloWorld/HelloWorldApp.swift
+++ Combine/Firebase/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 import FirebaseCore
+import FirebaseFirestore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     FirebaseApp.configure()
 
+    let settings = Firestore.firestore().settings
+    settings.host = "localhost:8080" // Or the IP address of your emulator host
+    settings.isSSLEnabled = false // Disable SSL for emulator connection
+    settings.cacheSettings = MemoryCacheSettings() // Or PersistentCacheSettings() if you need persistence
+
+    Firestore.firestore().settings = settings
+    
     return true
   }
 }
