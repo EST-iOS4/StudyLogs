509098f7a7c52b8edaa37fd8390056e0c2756570
배정만 <jungman82@gmail.com>
2025-09-25 13:19:19 +0900

    Threading 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 UI 구성 추가

---
+++ Combine/Threading/Threading/ThreadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadingApp.swift
+//  Threading
+//
+//  Created by Jungman Bae on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadingApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
