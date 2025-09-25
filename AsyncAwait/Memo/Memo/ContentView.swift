1ce35f5932c99910472911209096d6d02a58e463
배정만 <jungman82@gmail.com>
2025-09-25 15:34:43 +0900

    Memo 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 UI 구성 추가

---
+++ AsyncAwait/Memo/Memo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Memo
+//
+//  Created by Jungman Bae on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ AsyncAwait/Memo/Memo/MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoApp.swift
+//  Memo
+//
+//  Created by Jungman Bae on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
