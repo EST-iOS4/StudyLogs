c3cd2ad13daa65e52d83e35b0e96cae543c44345
배정만 <jungman82@gmail.com>
2025-09-26 09:33:08 +0900

    Retry 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 UI 구성 추가

---
+++ AsyncAwait/Retry/Retry/RetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetryApp.swift
+//  Retry
+//
+//  Created by Jungman Bae on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RetryApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
