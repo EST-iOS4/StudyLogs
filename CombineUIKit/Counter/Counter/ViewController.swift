391b5aabd2f846f5a546b31f4ca4a96bf4f8630d
배정만 <jungman82@gmail.com>
2025-09-29 09:41:14 +0900

    Main.storyboard 및 ViewController.swift 업데이트: 카운터 UI 구성 및 아울렛 연결 추가

--- CombineUIKit/Counter/Counter/ViewController.swift
+++ CombineUIKit/Counter/Counter/ViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var countLabel: UILabel!
+  @IBOutlet weak var incrementButton: UIButton!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
