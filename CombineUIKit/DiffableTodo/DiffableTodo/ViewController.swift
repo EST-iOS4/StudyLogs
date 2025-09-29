1a83d1af0a3f324e87f43a52c60fbf74f8f0c192
배정만 <jungman82@gmail.com>
2025-09-29 11:18:31 +0900

    DiffableTodo 프로젝트 초기 설정: Xcode 프로젝트 파일, AppDelegate, SceneDelegate, ViewController 및 스토리보드 추가

---
+++ CombineUIKit/DiffableTodo/DiffableTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiffableTodo
+//
+//  Created by Jungman Bae on 9/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Diffable Todo"
+  }
+
+
+}
+
