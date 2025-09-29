b07af93ecc02ebca0e4442ef262c0af8832e06a0
배정만 <jungman82@gmail.com>
2025-09-29 09:50:11 +0900

    (작업중) ViewController.swift 업데이트: 카운터 기능 구현 및 바인딩 설정 추가

--- CombineUIKit/Counter/Counter/ViewController.swift
+++ CombineUIKit/Counter/Counter/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
   @IBOutlet weak var countLabel: UILabel!
   @IBOutlet weak var incrementButton: UIButton!
-  
+
+  private var count = CurrentValueSubject<Int, Never>(0)
+  private var cancellables = Set<AnyCancellable>()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setupBindings()
+  }
+
+  private func setupBindings() {
+    count
+      .map { "카운트: \($0)" }
+      .assign(to: \.text, on: countLabel)
+      .store(in: &cancellables)
+
+    incrementButton.publisher
+      .sink { [weak self] event in
+        print("event: \(event.debugDescription)")
+        let currentValue = self?.count.value ?? 0
+        self?.count.send(currentValue + 1)
+      }
+      .store(in: &cancellables)
   }
 }
 
