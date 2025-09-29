36c18d49406f63d520c500fb0eef12ec9b7f4656
배정만 <jungman82@gmail.com>
2025-09-29 09:25:18 +0900

    Counter 프로젝트 초기 설정: Xcode 프로젝트 파일 및 기본 UI 구성 추가

---
+++ CombineUIKit/Counter/Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Jungman Bae on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
