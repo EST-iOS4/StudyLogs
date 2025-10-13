cacd1d592082c9b57a386a810f0950779f80c4f7
배정만 <jungman82@gmail.com>
2025-10-13 14:46:24 +0900

    XCTest, Swift Testing: 프로젝트 초기 설정 및 기본 파일 추가

---
+++ TDD/TestBasics/TestBasics/TestBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBasicsApp.swift
+//  TestBasics
+//
+//  Created by Jungman Bae on 10/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TestBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TDD/TestBasics/TestBasicsSwiftTestings/TestBasicsSwiftTestings.swift
@@ -0,0 +1,16 @@
+//
+//  TestBasicsSwiftTestings.swift
+//  TestBasicsSwiftTestings
+//
+//  Created by Jungman Bae on 10/13/25.
+//
+
+import Testing
+
+struct TestBasicsSwiftTestings {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
