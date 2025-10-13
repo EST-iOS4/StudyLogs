9b282441a5fa72a9d9820979c12aa4aa6d40ce84
배정만 <jungman82@gmail.com>
2025-10-13 10:07:08 +0900

    feat: fizzBuzz 함수 추가 및 입력에 따른 출력 변경

--- TDD/hello_world.swift
+++ TDD/hello_world.swift
@@ -1,14 +1,29 @@
 #!/usr/bin/env swift
 import Foundation
 
+func fizzBuzz(_ number: Int) -> String {
+  let divisibleBy3 = number % 3 == 0
+  let divisibleBy5 = number % 5 == 0
+
+  switch (divisibleBy3, divisibleBy5) {
+  case (false,false): return "\(number)"
+  case (true, false): return "fizz"
+  case (false, true): return "buzz"
+  case (true, true): return "fizz-buzz"
+  }
+}
+
 func main() {
   guard CommandLine.argc > 1 else {
     print("Hello, world!")
     return
   }
 
-  let name = CommandLine.arguments[1]
-  print("Hello, \(name)!")
+  if let number = Int(CommandLine.arguments[1]) {
+    print(fizzBuzz(number))
+  } else {
+    print("Hello, world!")
+  }
 }
 
 main()
