8a40be0cc77e59a62a304d3e15793ae66129b93e
배정만 <jungman82@gmail.com>
2025-10-14 10:25:28 +0900

    Albertos 프로젝트 생성

---
+++ TDD/Albertos/Albertos/AlbertosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbertosApp.swift
+//  Albertos
+//
+//  Created by Jungman Bae on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct AlbertosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
