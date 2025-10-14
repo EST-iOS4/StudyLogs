b2a5dda683e89340ba5772a7f08aa9001c37f06c
배정만 <jungman82@gmail.com>
2025-10-14 09:27:20 +0900

    Step2: 제품 배열 합계 계산 기능 및 관련 테스트 추가

--- TDD/TestBasics/TestBasics/Main.swift
+++ TDD/TestBasics/TestBasics/Main.swift
@@ -10,3 +10,12 @@ import Foundation
 func isLeap(_ year: Int) -> Bool {
   return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)
 }
+
+struct Product {
+  let category: String
+  let price: Double
+}
+
+func sumOf(_ products: [Product], withCategory category: String) -> Double {
+  return products.reduce(0.0) { $0 + $1.price }
+}
--- TDD/TestBasics/TestBasicsSwiftTestings/TestBasicsSwiftTestings.swift
+++ TDD/TestBasics/TestBasicsSwiftTestings/TestBasicsSwiftTestings.swift
@@ -9,6 +9,34 @@ import Testing
 
 struct TestBasicsSwiftTestings {
 
+  @Test("빈 제품 배열의 합은 0을 반환")
+  func sumOfEmptyArrayIsZero() {
+    // Arrange
+    let category = "books"
+    let products = [Product]()
+
+    // Act
+    let sum = sumOf(products, withCategory: category)
+
+    // Assert
+    #expect(sum == 0)
+  }
+
+  @Test("하나의 아이템의 합은 아이템의 가격을 반환")
+  func sumOfOneItemIsItemPrice() {
+    // Arrange
+    let category = "books"
+    let product = Product(category: category, price: 3)
+    let products = [product]
+
+    // Act
+    let sum = sumOf(products, withCategory: category)
+
+    // Assert
+    #expect(sum == product.price)
+
+  }
+
   @Test("4로 나누어 떨어지면 윤년")
   func evenlyDivisibleBy4IsLeap() {
     #expect(isLeap(2020))
