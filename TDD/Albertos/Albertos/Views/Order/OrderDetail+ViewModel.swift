3a4e9eb74ca7b272e36d6434baaa4f8b52bb085d
배정만 <jungman82@gmail.com>
2025-10-21 10:05:24 +0900

    feat: HippoPayments 프레임워크 추가 및 OrderDetail 뷰모델에 결제 프로세서 통합

--- TDD/Albertos/Albertos/Views/Order/OrderDetail+ViewModel.swift
+++ TDD/Albertos/Albertos/Views/Order/OrderDetail+ViewModel.swift
@@ -7,16 +7,21 @@
 
 import Foundation
 import Combine
+import HippoPayments
 
 extension OrderDetail {
   class ViewModel: ObservableObject {
+    private let paymentProcessor: HippoPaymentsProcessor
+    
     @Published private(set) var orderItems: [MenuItem] = []
     @Published private(set) var orderTotal: Double = 0.0
     private let orderController: OrderController
     private var cancellables = Set<AnyCancellable>()
 
-    init(orderController: OrderController) {
+    init(orderController: OrderController,
+         paymentProcessor: HippoPaymentsProcessor) {
       self.orderController = orderController
+      self.paymentProcessor = paymentProcessor
       setupSubscriptions()
     }
 
@@ -27,7 +32,7 @@ extension OrderDetail {
         }
         .store(in: &cancellables)
     }
-    
+
     private func updateOrderDetails(for order: Order) {
       orderItems = order.items
       orderTotal = order.total
