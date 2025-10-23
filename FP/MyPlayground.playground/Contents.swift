600a904e1379a2de267f907e683cb8a382729e2c
배정만 <jungman82@gmail.com>
2025-10-23 11:47:28 +0900

    feat: applyOperation 함수 추가 및 관련 예제 코드 작성

--- FP/MyPlayground.playground/Contents.swift
+++ FP/MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
+func applyOperation(_ numbers: [Int], operation: (Int) -> Int) -> [Int] {
+  return numbers.map(operation)
+}
+
+let values = [1,2,3,4,5]
+
+let doubled = applyOperation(values) { $0 * 2 }
+print("2배: \(doubled)")
+
+let squared = applyOperation(values) { $0 * $0 }
+print("제곱: \(squared)")
+
+let incremented = applyOperation(values) { $0 + 10 }
+print("10 증가: \(incremented)")
+
 // 명령형에서 선언형으로 변환
 
 let numbers = [1,2,3,4,5,6,7,8,9,10]
