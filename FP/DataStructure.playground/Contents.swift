48ab60341c2fc7dbb8dc5c46602e8d4b8358f5e4
배정만 <jungman82@gmail.com>
2025-10-28 10:16:22 +0900

    feat: Monoid 프로토콜 및 연산자 추가, String 및 Array에 대한 구현 추가

--- FP/DataStructure.playground/Contents.swift
+++ FP/DataStructure.playground/Contents.swift
@@ -9,6 +9,13 @@ extension Int: Semigroup {
   }
 }
 
+extension String: Semigroup {
+  func operation(_ element: String) -> String {
+    return self + element
+  }
+}
+
+
 extension Array: Semigroup {
   func operation(_ element: Array<Element>) -> Array<Element> {
     self + element
@@ -36,4 +43,37 @@ let z = 7
 let result1 = x.operation(y.operation(z))
 let result2 = (x.operation(y)).operation(z)
 print(result1 == result2)  // 출력: true (결합 법칙 만족)
+
+protocol Monoid: Semigroup {
+  static func identity() -> Self
+}
+
+extension Int: Monoid {
+  static func identity() -> Int {
+    return 0
+  }
+}
+
+extension String: Monoid {
+  static func identity() -> String {
+    return ""
+  }
+}
+
+extension Array: Monoid {
+  static func identity() -> Array<Element> {
+    return []
+  }
+}
+
+
+print((5 <> Int.identity()) == 5)
+
+func mconcat<M: Monoid>(_ elements: [M]) -> M {
+  return elements.reduce(M.identity(), <>)
+}
+
+print(mconcat([1,2,3,4,5]))
+print(mconcat(["Hello", " ", "Functional", " ", "Swift"]))
+
 
