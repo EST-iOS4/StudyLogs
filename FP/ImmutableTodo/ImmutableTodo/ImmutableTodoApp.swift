03130931509ea9c31cb678c83774e3a13e5c54cf
배정만 <jungman82@gmail.com>
2025-10-30 10:14:56 +0900

    불변 객체 실습: ImmutableTodo 프로젝트 파일 및 초기 구조 추가

---
+++ FP/ImmutableTodo/ImmutableTodo/ImmutableTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmutableTodoApp.swift
+//  ImmutableTodo
+//
+//  Created by Jungman Bae on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ImmutableTodoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
