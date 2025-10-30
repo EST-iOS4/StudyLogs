d5ffb449b6d92ca0a4bdc09dc8bbd0b7209562ea
배정만 <jungman82@gmail.com>
2025-10-30 14:46:49 +0900

    TCA 실습: TCACounter 프로젝트 초기 설정 및 기본 UI 구성 추가

---
+++ FP/TCACounter/TCACounter/TCACounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCACounterApp.swift
+//  TCACounter
+//
+//  Created by Jungman Bae on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TCACounterApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
