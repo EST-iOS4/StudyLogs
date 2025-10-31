1270d9231d2d6295da7842cbce58cb7443826fcf
배정만 <jungman82@gmail.com>
2025-10-31 14:29:28 +0900

    TCATodo 실습 프로젝트 생성

---
+++ FP/TCATodo/TCATodo/TCATodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATodoApp.swift
+//  TCATodo
+//
+//  Created by Jungman Bae on 10/31/25.
+//
+
+import SwiftUI
+
+@main
+struct TCATodoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ FP/TCATodo/TCATodoTests/TCATodoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCATodoTests.swift
+//  TCATodoTests
+//
+//  Created by Jungman Bae on 10/31/25.
+//
+
+import Testing
+@testable import TCATodo
+
+struct TCATodoTests {
+  
+  @Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+  }
+  
+}
